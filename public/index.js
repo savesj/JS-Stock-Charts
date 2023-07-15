0e7f815524ca6cad5c9b343315bd26c4d8426ce6
savesj <jesuslares.20@hotmail.com>
2023-07-14 23:45:27 -0400

    new variable declaration

--- public/index.js
+++ public/index.js
@@ -1,9 +1,13 @@
 async function main() {
-
     const timeChartCanvas = document.querySelector('#time-chart');
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
-}
+    const response = await fetch(`https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1day&apikey=b2b0e11651294ee9a789558a1625b754`)
 
-main()
+    const result = await response.json()
+
+    const { GME, MSFT, DIS, BNTX } = result;
+    const stocks = [GME, MSFT, DIS, BNTX];
+}
+main();
